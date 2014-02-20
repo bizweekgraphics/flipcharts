9db3209bbec28728ffd732b4903e4bae404eafe7
Toph Tucker <tophtucker@gmail.com>
2014-02-20 11:58:57 -0500

    fluid-scrubber: tilt to scrub works on iphone. working on zoom.

--- js/fluid-scrubber.js
+++ js/fluid-scrubber.js
@@ -1,6 +1,7 @@
-var xExtent, yExtent, linePath;
+var xExtent, yExtent, linePath,
+    scrubX, scrubY;
 
-var defaultMargin = $(document).width()*0.05;
+var defaultMargin = $(document).width()*0.1;
 
 var margin = {top: defaultMargin, right: defaultMargin, bottom: defaultMargin, left: defaultMargin},
     width = $(document).width() - margin.left - margin.right,
@@ -14,15 +15,21 @@ var y = d3.scale.linear()
     .domain([0,100])
     .range([height,0]);
     
-var xZoom = d3.scale.linear()
+var xZoomMouse = d3.scale.linear()
     .domain([0,height])
     .range([1,10]);
 
-var xPan = d3.scale.linear()
+var xZoomTilt = d3.scale.linear()
+    .domain([-10,10]) //degrees of tilt
+    .range([1,10]);
+
+var xPanMouse = d3.scale.linear()
     .domain([0,width])
     .range([0, 2000]);
 
-console.log("Width: "+width);
+var xPanTilt = d3.scale.linear()
+    .domain([10,-10]) //degrees of tilt
+    .range([0, 2000]);
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -74,7 +81,8 @@ d3.tsv("data/population.tsv", function(error, data) {
   gx.call(xAxis);
   gy.call(yAxis);
   
-  xPan.range(d3.extent(data, function(d) { return d.year; }));
+  xPanMouse.range(d3.extent(data, function(d) { return d.year; }));
+  xPanTilt.range(d3.extent(data, function(d) { return d.year; }));
   
   linePath = svg.append("path")
       .datum(data)
@@ -88,17 +96,52 @@ d3.select("body").on("mousemove", function(d,i) {
   // NOTE: The outer wrapper of min/max below keeps the lower bound of the domain from crossing over past the upper, and vice versa. 
   // That's a problem when the chart margins are bigger than the +/- range below, and the whole scale flips backwards. Funky.
   // But you also get a nice springy thing as long as you stay on the near side of that asymptote. So they may be some use for it.
+  
+  scrubX = xPanMouse(d3.mouse(svg.node())[0]);
+  scrubY = xZoomMouse(d3.mouse(svg.node())[0]);
+  scrub(scrubX);
+  
+});
+
+
+if (window.DeviceOrientationEvent) {
+
+  // Listen for the deviceorientation event and handle the raw data
+  window.addEventListener('deviceorientation', function(eventData) {
+    gamma 	= eventData.gamma;	// - left-to-right + (degrees)
+    beta 	  = eventData.beta;	  // - back-to-front + (degrees)
+    alpha 	= eventData.alpha	  // compass direction (degrees)
+          
+    if(gamma == null || beta == null || alpha == null) {
+      // desktopMode();
+    } else {
+      
+      scrubX = xPanTilt(gamma);
+      scrub(scrubX);
+      
+    }
+  }, false);
+} 
+else {
+  // desktopMode();
+}
+
+function scrub(scrubX) {
+  
+  
+  // NOTE: The outer wrapper of min/max below keeps the lower bound of the domain from crossing over past the upper, and vice versa. 
+  // That's a problem when the chart margins are bigger than the +/- range below, and the whole scale flips backwards. Funky.
+  // But you also get a nice springy thing as long as you stay on the near side of that asymptote. So they may be some use for it.
   x.domain(
     [
-      Math.min(Math.max( xPan(d3.mouse(svg.node())[0]) - 500, xExtent[0] ), xExtent[1] - 500),
-      Math.max(Math.min( xPan(d3.mouse(svg.node())[0]) + 500, xExtent[1] ), xExtent[0] + 500)
+      Math.min(Math.max(scrubX - 500, xExtent[0] ), xExtent[1] - 500),
+      Math.max(Math.min(scrubX + 500, xExtent[1] ), xExtent[0] + 500)
     ]
   );
-  
-  console.log(Math.max( xPan(d3.mouse(svg.node())[0]) - 5, xExtent[0] ));
-  
+    
   xAxis.tickValues(x.domain());
   
   gx.call(xAxis);
   linePath.attr("d", line);
-});
+  
+}
