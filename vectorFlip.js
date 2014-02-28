6d787ceef8fc4a99826d3762a2e3a2cc92e1c901
Toph Tucker <tophtucker@gmail.com>
2014-02-28 11:49:11 -0500

    man atom's great. lol i dunno what im doin

--- vectorFlip.js
+++ vectorFlip.js
@@ -9,23 +9,38 @@ var data = [
   }
 ];
 
+var xExt = d3.extent(data, function(d) { return d.x; });
+var yExt = d3.extent(data, function(d) { return d.y; });
+
 var xScale = d3.scale.linear()
-  .domain([0,d3.max(data, function(d) { return d.x; })])
+  .domain([0,xExt[1]])
   .range($("#container").width());
 
 var yScale = d3.scale.linear()
-  .domain([0,d3.max(data, function(d) { return d.y; })])
+  .domain([0,yExt[1]])
   .range($("#container").height());
 
 var xInterpolate = d3.scale.linear()
   .domain([0,1])
-  .range(d3.extent(data, function(d) { return d.x; }));
+  .range(xExt);
 
 var yInterpolate = d3.scale.linear()
   .domain([0,1])
-  .range(d3.extent(data, function(d) { return d.y; }));
+  .range(yExt);
+
+var xLabel = svg.append("text")
+  .text("Box office revenue")
+  .attr("x", xScale(xExt[1]))
+  .attr("y", yScale(yExt[0]));
+var yLabel = svg.append("text")
+  .text("Critical acclaim")
+  .attr("x", xScale(xExt[0]))
+  .attr("y", yScale(yExt[1]));
 
 function changeSlide(scrubX) {
   var xVector = drawArrow(parent,[0,0],[xScale(xInterpolate(scrubX))],0,false);
   var yVector = drawArrow(parent,[0,0],[0,yScale(yInterpolate(scrubX))],false);
+
+  var xLabel.style("opacity", scrubX);
+  var yLabel.style("opacity", 1-scrubX);
 }
