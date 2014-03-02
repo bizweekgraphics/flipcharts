0f5e75450f6050c54525f07f5ace02e2c49e328c
Toph Tucker <tophtucker@gmail.com>
2014-03-02 11:45:00 -0500

    not gonna label like this

--- vectorFlip.js
+++ vectorFlip.js
@@ -28,19 +28,8 @@ var yInterpolate = d3.scale.linear()
   .domain([0,1])
   .range(yExt);
 
-var xLabel = svg.append("text")
-  .text("Box office revenue")
-  .attr("x", xScale(xExt[1]))
-  .attr("y", yScale(yExt[0]));
-var yLabel = svg.append("text")
-  .text("Critical acclaim")
-  .attr("x", xScale(xExt[0]))
-  .attr("y", yScale(yExt[1]));
-
 function changeSlide(scrubX) {
   var xVector = drawArrow(parent,[0,0],[xScale(xInterpolate(scrubX))],0,false);
   var yVector = drawArrow(parent,[0,0],[0,yScale(yInterpolate(scrubX))],false);
 
-  var xLabel.style("opacity", scrubX);
-  var yLabel.style("opacity", 1-scrubX);
 }
