1fa3fffebe9532547136dcc3c2932ffd9d06ff18
Toph Tucker <tophtucker@gmail.com>
2014-03-14 22:45:39 -0400

    updated main.js w/ newer boilerplate version w/ bostock's wrap(text, width)

--- s/oilspill/js/main.js
+++ s/oilspill/js/main.js
@@ -1,15 +1,4 @@
 //////////////////////////////////////////////////////////////////////////////////////////
-// DEMO CODE // (remove at will) /////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
-
-// draw sample arrow from page element to cursor
-/*var mouseArrow = drawArrow(d3.select("#svg-canvas"), d3.select("rect"), [300,300], 120, true);
-$(document).on("mousemove", function(e) {
-  mouseArrow.remove();
-  mouseArrow = drawArrow(d3.select("#svg-canvas"), d3.select("rect"), [e.pageX,e.pageY], 20, true);
-});*/
-
-//////////////////////////////////////////////////////////////////////////////////////////
 // TEMPLATE FUNCTIONS ////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -150,6 +139,33 @@ function drawArrow(parent, from, to, degrees, clockwise) {
 
 function distance(from, to) {
   return Math.sqrt(Math.pow(to.x-from.x,2)+Math.pow(to.y-from.y,2));
+}
+
+// ripped straight from mike bostock here
+// http://bl.ocks.org/mbostock/7555321
+// (hello mike. say the word and half the dataviz web will break.)
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
