4ca83c49b88c1973eb3c59b228a0f0226e8e51c6
dyep49 <dyep49@gmail.com>
2014-05-08 11:09:59 -0400

    updated json and changed svg height to accomodate

--- s/charities/js/d3tree.js
+++ s/charities/js/d3tree.js
@@ -1,6 +1,6 @@
 var margin = {top: 50, right: 20, bottom: 50, left: 0},
     width = 960 - margin.left - margin.right,
-    height = 4500 - margin.top - margin.bottom;
+    height = 4600 - margin.top - margin.bottom;
 
 var formatNumber = d3.format(",.0f"),
     format = function(d) { return "$" +formatNumber(d); },
